ef2d4ab8627f9c460b5d8662c6e846b6eba5c0a1
tonis <c.cartes07@ufromail.cl>
2021-09-28 00:25:50 -0300

    commit

--- src/main/java/Eje.java
+++ src/main/java/Eje.java
@@ -1,5 +1,42 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Eje {
     public static void main(String[] args) {
 
+    }
+
+    public static void inicio(){
+        final String nombreDeArchivo = "./files/archivo.csv";
+        crearArchivoCSV(nombreDeArchivo);
+    }
+
+    private static void crearArchivoCSV(String nombreDeArchivo) {
+        // this also can be "\t" for tab delimitador
+        crearArchivoCSV(nombreDeArchivo, ",");
+    }
+
+    private static void crearArchivoCSV(String file, String delim) {
+        final String NEXT_LINE = "\n";
+        try {
+            FileWriter fw = new FileWriter(file);
+
+            fw.append("testing").append(delim);
+            fw.append("123").append(NEXT_LINE);
+
+            fw.append("value1");
+            fw.append(delim);
+            fw.append("312");
+            fw.append(NEXT_LINE);
+
+            fw.append("anotherthing,888\n");
+
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            // Error al crear el archivo, por ejemplo, el archivo
+            // está actualmente abierto.
+            e.printStackTrace();
+        }
     }
 }
